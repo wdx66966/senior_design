f8fff6ce34564ea8a145c9766930c87ac5ef6668
wdx66966 <60238094+wdx66966@users.noreply.github.com>
2021-04-30 09:16:15 -0700

    Update create_lmdb.py

--- create_lmdb.py
+++ create_lmdb.py
@@ -57,7 +57,7 @@ test_data = [img for img in glob.glob("../input/validation/*jpg")]
 #Shuffle train_data
 random.shuffle(train_data)
 
-print 'Creating train_lmdb'
+print ("Creating train_lmdb")
 
 in_db = lmdb.open(train_lmdb, map_size=int(1e12))
 with in_db.begin(write=True) as in_txn:
@@ -86,7 +86,7 @@ with in_db.begin(write=True) as in_txn:
 in_db.close()
 
 
-print '\nCreating validation_lmdb'
+print ("\nCreating validation_lmdb")
 
 in_db = lmdb.open(validation_lmdb, map_size=int(1e12))
 with in_db.begin(write=True) as in_txn:
@@ -95,7 +95,7 @@ with in_db.begin(write=True) as in_txn:
             continue
         img = cv2.imread(img_path, cv2.IMREAD_COLOR)
         img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
-         if 'angry' in img_path:
+        if 'angry' in img_path:
             label = 0
         elif 'disgust' in img_path:
             label = 1
@@ -111,7 +111,7 @@ with in_db.begin(write=True) as in_txn:
             label = 6
         datum = make_datum(img, label)
         in_txn.put('{:0>5d}'.format(in_idx), datum.SerializeToString())
-        print '{:0>5d}'.format(in_idx) + ':' + img_path
+        print ('{:0>5d}'.format(in_idx) + ':' + img_path)
 in_db.close()
 
-print '\nFinished processing all images'
+print ("\nFinished processing all images")
