fc27be60678c4020d15e27919a9bc1f6786d8846
wdx66966 <60238094+wdx66966@users.noreply.github.com>
2021-07-16 20:37:40 -0700

    Update create_lmdb.py

--- FE/create_lmdb.py
+++ FE/create_lmdb.py
@@ -1,41 +1,29 @@
-'''
-Title           :create_lmdb.py
-Description     :This script divides the training images into 2 sets and stores them in lmdb databases for training and validation.
-Author          :Adil Moujahid
-Date Created    :20160619
-Date Modified   :20160625
-version         :0.2
-usage           :python create_lmdb.py
-python_version  :2.7.11
-'''
-
 import os
 import glob
 import random
 import numpy as np
-
 import cv2
-
 import caffe
 from caffe.proto import caffe_pb2
 import lmdb
 
-#Size of images
-IMAGE_WIDTH = 48
-IMAGE_HEIGHT = 48
+#Size of image
+IMAGE_WIDTH = 227
+IMAGE_HEIGHT = 227
+
 
 def transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT):
 
     #Histogram Equalization
-    img[:, :, 0] = cv2.equalizeHist(img[:, :, 0])
-    img[:, :, 1] = cv2.equalizeHist(img[:, :, 1])
-    img[:, :, 2] = cv2.equalizeHist(img[:, :, 2])
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    img2 = np.zeros_like(img)
+    img2[:,:,0] = gray
+    img2[:,:,1] = gray
+    img2[:,:,2] = gray
     #Image Resizing
-    img = cv2.resize(img, (img_width, img_height), interpolation = cv2.INTER_CUBIC)
+    img2 = cv2.resize(img, (img_width, img_height), interpolation = cv2.INTER_CUBIC)
 
-    return img
-
-
+    return img2
 def make_datum(img, label):
     #image is numpy.ndarray format. BGR instead of RGB
     return caffe_pb2.Datum(
@@ -45,57 +33,55 @@ def make_datum(img, label):
         label=label,
         data=np.rollaxis(img, 2).tostring())
 
-train_lmdb = '/home/ubuntu/senior_design/FE/facial_expression/input/train_lmdb'
-validation_lmdb = '/home/ubuntu/senior_design/FE/facial_expression/input/validation_lmdb'
+#train_lmdb = '/home/ubuntu/senior_design/FE/facial_expression/input/train_lmdb'
+#validation_lmdb = '/home/ubuntu/senior_design/FE/facial_expression/input/validation_lmdb'
+train_lmdb = '/home/ubuntu/senior_design/FE/CK+_JAFFE/input/train_lmdb'
+validation_lmdb = '/home/ubuntu/senior_design/FE/CK+_JAFFE/input/validation_lmdb'
+
 
 os.system('rm -rf  ' + train_lmdb)
 os.system('rm -rf  ' + validation_lmdb)
 
-
-train_data = [img for img in glob.glob("*/input/train/*/*jpg")]
-test_data = [img for img in glob.glob("*/input/validation/*/*jpg")]
+train_data = [img for img in glob.glob("CK+_JAFFE/input/train/*/*")]
+test_data = [img for img in glob.glob("CK+_JAFFE/input/validation/*/*")]
 
 #Shuffle train_data
 random.shuffle(train_data)
 
 print ("Creating train_lmdb")
-
+x=0
 in_db = lmdb.open(train_lmdb, map_size=int(1e12))
 with in_db.begin(write=True) as in_txn:
     for in_idx, img_path in enumerate(train_data):
-        if in_idx %  6 == 0:
-            continue
         img = cv2.imread(img_path, cv2.IMREAD_COLOR)
         img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
-        if 'angry' in img_path:
+        if 'anger' in img_path:
             label = 0
         elif 'disgust' in img_path:
             label = 1
         elif 'fear' in img_path:
             label = 2
-        elif 'happy' in img_path:
+        elif 'happiness' in img_path:
             label = 3
         elif 'neutral' in img_path:
             label = 4
-        elif 'sad' in img_path:
+        elif 'sadness' in img_path:
             label = 5
         else:
             label = 6
+        x=x+1
         datum = make_datum(img, label)
         in_txn.put('{:0>5d}'.format(in_idx).encode(), datum.SerializeToString())
 in_db.close()
-
-
+print (x)
 print ("\nCreating validation_lmdb")
 
 in_db = lmdb.open(validation_lmdb, map_size=int(1e12))
 with in_db.begin(write=True) as in_txn:
     for in_idx, img_path in enumerate(test_data):
-        if in_idx % 6 != 0:
-            continue
         img = cv2.imread(img_path, cv2.IMREAD_COLOR)
         img = transform_img(img, img_width=IMAGE_WIDTH, img_height=IMAGE_HEIGHT)
-        if 'angry' in img_path:
+        if 'anger' in img_path:
             label = 0
         elif 'disgust' in img_path:
             label = 1
@@ -113,4 +99,5 @@ with in_db.begin(write=True) as in_txn:
         in_txn.put('{:0>5d}'.format(in_idx).encode(), datum.SerializeToString())
 in_db.close()
 
-print ("\nFinished processing all images")
+
+
